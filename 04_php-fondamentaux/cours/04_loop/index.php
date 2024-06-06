c202a955170088c295a72a9eaac2010bb09ca787
Prydwen-kun <tengokunokokoro@gmail.com>
2024-06-06 14:51:48 +0200

    add02
    aahaha

--- 04_php-fondamentaux/cours/04_loop/index.php
+++ 04_php-fondamentaux/cours/04_loop/index.php
@@ -1,2 +1,10 @@
 <?php
 #COURS LOOP
+
+
+$array = ['name0' => 'jean','name1'=>'michel'];
+
+foreach ($array as $key => $value) {
+    # code...
+    echo $key.' : '.$value.PHP_EOL;
+}
--- 04_php-fondamentaux/tp/04_loop/index.php
+++ 04_php-fondamentaux/tp/04_loop/index.php
@@ -9,3 +9,19 @@ $i++;
 if($i<3)
 goto a;
 
+//EXOS
+
+#1
+#2
+#3
+#4
+#5
+
+
+
+$array = ['name0' => 'jean','name1'=>'michel'];
+
+foreach ($array as $key => $value) {
+    # code...
+    echo $key.' : '.$value.PHP_EOL;
+}
