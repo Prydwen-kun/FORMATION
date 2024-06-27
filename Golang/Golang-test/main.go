125f578d79bcda0744127313b57110de5e2402f1
Prydwen-kun <tengokunokokoro@gmail.com>
2024-06-27 12:31:16 +0200

    goadd

---
+++ Golang/Golang-test/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+	var (
+		tableau[] int
+		number float32 = 6.2
+	)
+	tableau = append(tableau, 1,2,3)
+	for i := 0; i < len(tableau); i++ {
+		println(number)
+		println(tableau[i])
+	}
+
+
+
+}
