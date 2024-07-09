fdcbc155ffdccd4db5589cc498b33fedcf7f721d
Prydwen-kun <tengokunokokoro@gmail.com>
2024-07-09 20:19:15 +0200

    livrehero

--- 04_php-fondamentaux/tp/LivreOuVousEtesLeHeros/index.php
+++ 04_php-fondamentaux/tp/LivreOuVousEtesLeHeros/index.php
@@ -1,18 +1,50 @@
 <?php
+$currentPage = "Le livre dont vous êtes le héros";
+
 include "inc/head.php";
 
 include "data/data.php";
 include "function/function.php";
 
 session_start();
+if (!isset($_SESSION['storyIndex'])) {
+    $_SESSION['storyIndex'] = 0;
+} else if (isset($_POST['goto'])) {
+    $_SESSION['storyIndex'] = $_POST['goto'];
+}
+
 
 ?>
-<header class="header">
+<header class="header mt-3">
     <h1>Le Livre dont vous êtes le héro</h1>
 </header>
 <section class="container mt-5">
-   
-               
+    <h2>Chapitre <?= $_SESSION['storyIndex'] ?></h2>
+    <div class="container mt-3">
+        <?php
+        if (isset($_SESSION['storyIndex'])) {
+            echo $story[$_SESSION['storyIndex']]['text'];
+        }
+
+        ?>
+    </div>
+    <div class="container mt-3">
+        <form action="index.php?storyPlay=true" method="post" id="form1">
+            <?php foreach ($story[$_SESSION['storyIndex']]['choice'] as $key => $choice): ?>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="goto" value="<?= $choice['goto'] ?>"
+                        id="choice<?= $key ?>">
+                    <label class="form-check-label" for="choice<?= $key ?>">
+                        <?= $choice['text'] ?>(Aller au chapitre <?= $choice['goto'] ?>)
+                    </label>
+                </div>
+            <?php endforeach; ?>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <div class="container mt-3">
+        <a href="./log/logout.php">Effacer progression</a>
+    </div>
 </section>
 
 <?php
--- 04_php-fondamentaux/tp/LivreOuVousEtesLeHeros/log/logout.php
+++ 04_php-fondamentaux/tp/LivreOuVousEtesLeHeros/log/logout.php
@@ -1,15 +1,20 @@
 <?php
+$currentPage = "Logout";
 include "../inc/head.php";
 
 session_start();
-
+if (isset($_SESSION['storyIndex'])) {
+    unset($_SESSION['storyIndex']);
+}
+$_SESSION = [];
+session_destroy();
 
 ?>
 
 
 <section class="container mt-5">
-<p>Deleted groceries list !</p>
-<a href="../index.php?listDelete=true">Retour vers liste</a>
+<p>Back to 0 !</p>
+<a href="../index.php">Retour</a>
 </section>
 
 <?php
