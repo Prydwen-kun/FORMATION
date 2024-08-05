6bb596d3a27562fe396235de4ac2c185a9108633
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-08-05 12:12:44 +0200

    Update bdd_entreprise_03.sql

--- 06_SQL/01_sql/bdd_entreprise_03.sql
+++ 06_SQL/01_sql/bdd_entreprise_03.sql
@@ -57,11 +57,108 @@ FROM
     employe
     LEFT JOIN service USING (idService)
 WHERE
-    (
+    service.idService NOT IN (
         SELECT
-            COUNT(employe.idService)
+            employe.idService
         FROM
             employe
-    ) = 0
+    )
 ORDER BY
     nomComplet ASC;
+
+-- 6
+SELECT
+    CONCAT (employe.nom, ' ', employe.prenom) AS nomComplet,
+    service.nom serv
+FROM
+    employe
+    LEFT JOIN service USING (idService)
+WHERE
+    employe.idService IS NULL
+UNION
+SELECT
+    CONCAT (employe.nom, ' ', employe.prenom) AS nomComplet,
+    service.nom serv
+FROM
+    employe
+    LEFT JOIN service USING (idService)
+WHERE
+    service.idService NOT IN (
+        SELECT
+            employe.idService
+        FROM
+            employe
+    )
+ORDER BY
+    nomComplet ASC;
+
+-- 7
+SELECT
+    `employe`.`prenom` emp,
+    `responsable`.`prenom` resp
+FROM
+    employe
+    LEFT JOIN service ON employe.idService = service.idService
+    LEFT JOIN responsable ON service.idResponsable = responsable.idResponsable
+ORDER BY
+    emp ASC;
+
+-- 8
+SELECT DISTINCT
+    ROUND(
+        (
+            SELECT
+                AVG(salaire)
+            FROM
+                employe
+            WHERE
+                sexe = 'M'
+        ) - (
+            SELECT
+                AVG(salaire)
+            FROM
+                employe
+            WHERE
+                sexe = 'F'
+        ),
+        1
+    ) diff
+FROM
+    employe;
+
+-- 9
+SELECT
+    service.nom AS nomService,
+    COUNT(
+        CASE
+            WHEN employe.salaire < avg_salaire.average_salaire THEN 1
+        END
+    ) AS nb
+FROM
+    employe
+    LEFT JOIN service ON employe.idService = service.idService
+    CROSS JOIN (
+        SELECT
+            AVG(salaire) AS average_salaire
+        FROM
+            employe
+    ) AS avg_salaire
+GROUP BY
+    nomService
+ORDER BY
+    nomService ASC;
+
+-- 10
+SELECT
+    service.nom,
+    nbs.nb
+FROM
+    service
+    CROSS JOIN (
+        SELECT
+            COUNT(*) nb
+        FROM
+            employe
+    ) AS nbs
+ORDER BY
+    service.nom ASC;
