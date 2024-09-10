c7a92625697d610d5e92f396c3b4f4f307a2088d
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-09-10 17:05:59 +0200

    co1

--- PDO_valid/function/connexion/connexionBDD.php
+++ PDO_valid/function/connexion/connexionBDD.php
@@ -8,3 +8,68 @@ try {
 } catch (PDOException $e) {
     echo '<pre>Erreur connexion DB ! ' . $e . '</pre>';
 }
+
+function connexionAdmin($dbh)
+{
+    //CONNEXION
+    if (isset($_POST['user'], $_POST['password'])) {
+
+        $u_id = $_POST['user'];
+        $password = $_POST['password'];
+
+        try {
+            $query = "SELECT * 
+                     FROM user
+                     WHERE email = :u_id";
+
+            if (($req = $dbh->prepare($query))) {
+                if (($req->bindValue(':u_id', $u_id))) {
+                    if ($req->execute()) {
+                        $res = $req->fetchAll(PDO::FETCH_ASSOC);
+                    } else {
+                        echo 'Erreur requête !';
+                    }
+                } else {
+                    echo 'Value bind error !';
+                }
+                $req->closeCursor();
+                if ($u_id == $res['email'] && password_verify($password, $res['password'])) {
+                    $_SESSION['connected'] = true;
+                    $_SESSION['u_id'] = $u_id;
+                } else {
+                    header("Location: ?login=false");
+                }
+            } else {
+                echo 'Request prepare error !';
+            }
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+}
+
+function addDwarf($dbh,$post){
+    try {
+        $query = "INSERT INTO nain 
+        VALUES(DEFAULT,:nom,:barbe,:groupe)";
+
+        if (($req = $dbh->prepare($query))) {
+            if (
+                ($req->bindValue(':nom', $post['nom'])) &&
+                ($req->bindValue(':barbe', $post['barbe'])) &&
+                ($req->bindValue(':groupe', $post['groupeNum']))
+            ) {
+                if ($req->execute()) {
+                    return true;
+                } else {
+                    echo 'Erreur requête !';
+                }
+            } else {
+                echo 'Value bind error !';
+            }
+        } else {
+            echo 'Request prepare error !';
+        }
+    } catch (PDOException $e) {
+    }
+}
--- PDO_valid/include/header.php
+++ PDO_valid/include/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $currentPage ?></title>
     <link rel="stylesheet" href="asset/style/style.css">
+    <link rel="stylesheet" href="../../asset/style/style.css">
+
 </head>
 
 <body>
@@ -17,6 +19,12 @@
                 <li><a href="listeVille.php">Ville</a></li>
                 <li><a href="listeGroupe.php">Groupe</a></li>
                 <li><a href="listeTaverne.php">Taverne</a></li>
+                <?php if (isset($_SESSION['user']['connected']) && $_SESSION['user']['connected'] == 'true') {
+                    echo '<li><a href="sessionClose.php" class="connexionButton">Déconnexion</a></li>';
+                } else {
+                    echo '<li><a href="./function/connexion/verifConnexionUser.php" class="connexionButton">Connexion</a></li>';
+                } ?>
+
             </ul>
         </nav>
     </header>
