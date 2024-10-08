f57c515de4be2b1e2153d204ecfcfbe765bb70e1
Prydwen-kun <tengokunokokoro@gmail.com>
2024-10-08 17:44:19 +0200

    Create dashboardView.php

---
+++ MVC-Plateforme-offre-emploi/views/dashboardView.php
@@ -0,0 +1,16 @@
+<?php
+$page = 'Dashboard';
+include 'views/partials/head.php'; 
+?>
+
+<div class="section">
+    <h1 class="title">?Dashboard?</h1>
+    <div class="card is-shadowless">
+        <div class="card-content">
+            content
+        </div>
+    </div>
+</div>
+<!-- Main content goes here -->
+
+<?php include 'views/partials/foot.php'; ?>
